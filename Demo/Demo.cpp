0937e94ef27ed2a97c339a56f257cab8998bd743
Anserw <anserw.x@gmail.com>
2015-10-12 01:53:23 +0800

    Create demo for numerical_analysis

---
+++ Demo/Demo.cpp
@@ -0,0 +1,24 @@
+// Demo.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "../numerical_analysis/na_algorithm.h"
+#include <iostream>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	double l_mat_A[9] = { 8.1, 2.3, -1.5, 0.5, -6.23, 0.87, 2.5, 1.5, 10.2 };
+	double l_mat_b[3] = { 6.1, 2.3, 1.8 };
+	Mat A(3, 3);
+	Mat b(1, 3);
+	Mat x(1, 3);
+	A.init(l_mat_A);
+	b.init(l_mat_b);
+	solveLSEwithSGE(A, x, b);
+	cout << x[0] << endl << x[1] << endl << x[2] << endl;
+	system("pause");
+	return 0;
+}
+
