6a3407eee00306795c44a88dd093aca80ac76c13
Anserw <anserw.x@gmail.com>
2015-10-11 16:10:13 +0800

    overload operator + for Mat

---
+++ numerical_analysis/Mat.cpp
@@ -0,0 +1,50 @@
+#include "stdafx.h"
+#include "Mat.h"
+
+using namespace na;
+
+Mat::Mat()
+{
+}
+
+Mat::Mat(int w, int h)
+{
+	width = w;
+	height = h;
+	data = new double[w*h];
+}
+
+
+Mat::~Mat()
+{
+}
+
+bool Mat::init(double* input_data)
+{
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			data[getIndex(i,j)] = input_data[getIndex(i,j)];
+		}
+	}
+	return true;
+}
+
+int Mat::getIndex(int x, int y)
+{
+	return y*width + x;
+}
+
+
+Mat operator+(const Mat& a, const Mat& b)
+{
+	if (a.width != b.width || a.height != b.height) {
+		std::cout << "Error: Can't add them!" << std::endl;
+		return Mat();
+	}else {
+		Mat ret(a.width, a.height);
+		for (int i = 0; i < a.width*a.height; i++) {
+			ret.data[i] = a.data[i] + b.data[i];
+		}
+		return ret;
+	}
+}
---
+++ numerical_analysis/Mat.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+
+namespace na {
+	class Mat
+	{
+	public:
+		Mat();
+		Mat(int w, int h);
+		~Mat();
+
+		bool init(double *input_data);
+
+		inline int getIndex(int x, int y);
+
+		friend Mat operator +(const Mat& a, const Mat& b);
+
+	public:
+		int width, height;
+		double *data;
+	private:
+		
+		
+	};
+
+}
+
+
