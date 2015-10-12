0088664fdffa563a67475e98d2a4fd272397c4b2
Anserw <anserw.x@gmail.com>
2015-10-12 17:09:57 +0800

    new test case for 列主元素高斯消去法

--- unit_test/na_algorithm_UT.cpp
+++ unit_test/na_algorithm_UT.cpp
@@ -33,6 +33,21 @@ TEST(solveLSEwithSGETest, solve3x3problem2)
 	EXPECT_NEAR(5.546, x[2], 0.0005);
 }
 
+TEST(solveLSEwithCMEGETest, solve3x3problem1)
+{	
+	Mat A(3, 3);
+	Vec b(3);
+	Vec x(3);
+	A.init("8.1, 2.3, -1.5, 0.5, -6.23, 0.87, 2.5, 1.5, 10.2");
+	EXPECT_DOUBLE_EQ(10.2, A[2][2]);
+	b.init("6.1, 2.3, 1.8 ");
+	EXPECT_DOUBLE_EQ(1.8, b[2]);
+	EXPECT_TRUE(solveLSEwithCMEGE(A, x, b));
+	EXPECT_NEAR(0.8409, x[0], 0.0005);
+	EXPECT_NEAR(-0.2997, x[1], 0.0005);
+	EXPECT_NEAR(0.01415, x[2], 0.0005);
+}
+
 TEST(solveLinearSimultaneousEquationsTest, solve3x3problemWithSequentialGaussianElimination)
 {
 	double l_mat_A[9] = { 0.012, 0.01, 0.167, 1, 0.8334, 5.91, 3200, 1200, 4.2 };
